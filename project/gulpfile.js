5f6392e66fa477a692b2ac3b275a28c6db2074fa
kwu <kengcheng.wu@gmail.com>
2015-12-17 17:51:36 +0800

    add sass

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -12,39 +12,58 @@ gulp.task('js', function () {
    return gulp.src(appSrcPath+'/js/src/*.js')
 			  .pipe(plugins.concat('app.js'))
 			  .pipe(gulp.dest(appSrcPath+'/js/dist'))
-			  .pipe(plugins.rename('app.min.js'))
+			  .pipe(plugins.rename({extname:'.min.js'}))
 			  .pipe(plugins.uglify())
 			  .pipe(gulp.dest(appSrcPath+'/js/dist'))
 			  ;
 });
 
+//SCSS
+gulp.task('scss', function () {
+   return gulp.src(appSrcPath+'/scss/src/app.scss')
+        .pipe(plugins.sass())
+        .pipe(gulp.dest(appSrcPath+'/scss/dist'))
+        ;
+});
+
+//CSS
+gulp.task('css', ['scss'], function () {
+   return gulp.src([appSrcPath+'/css/src/*.css',appSrcPath+'/scss/dist/*.css'])
+          .pipe(plugins.concat('app.css'))
+          .pipe(gulp.dest(appSrcPath+'/css/dist'))
+          .pipe(plugins.minifyCss({keepSpecialComments: 0}))
+          .pipe(plugins.rename({extname:'.min.css'}))
+          .pipe(gulp.dest(appSrcPath+'/css/dist'))
+          ;
+});
 
 // Clean
 gulp.task('clean', function() {
-  return del([appTargetPath+'/*','!'+appTargetPath+'/META-INF','!'+appTargetPath+'/WEB-INF'],{force:true});
+  return del([appSrcPath+'/js/dist/*',
+              appSrcPath+'/css/dist/*',
+              appSrcPath+'/scss/dist/*',
+              appTargetPath+'/*',
+              '!'+appTargetPath+'/META-INF',
+              '!'+appTargetPath+'/WEB-INF'],{force:true});
 });
 
 //Build
-gulp.task('build', ['clean','js'], function() {
-  return gulp.src([appSrcPath+'/**','!'+appSrcPath+'/js/{src,src/**}'])
+gulp.task('build', ['clean','js','css'], function() {
+  return gulp.src([appSrcPath+'/**',
+                   '!'+appSrcPath+'/js/{src,src/**}',
+                   '!'+appSrcPath+'/css/{src,src/**}',
+                   '!'+appSrcPath+'/{scss,scss/**}'])
   			 	  .pipe(gulp.dest(appTargetPath))
   			 	  ;
 });
 
 
 //Watch
-gulp.task('watch', ['connect'], function () {
-   gulp.watch(appSrcPath+'/js/src/*.js', ['build']);
+gulp.task('watch', function () {
+   gulp.watch([appSrcPath+'/js/src/*.js',
+               appSrcPath+'/css/src/*.css',
+               appSrcPath+'/scss/src/*.scss'], ['build']);
 });
-
-
-//Connect
-// gulp.task('connect', function() {
-  // plugins.connect.server({
-  // 	root: appSrcPath,
-  // 	port: 8081,
-  // });
-// });
  
 
 gulp.task('default', ['build'], function() {});
